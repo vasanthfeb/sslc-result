035f235ee90fced3583b92c6d787b15f27ba894b
vasanthfeb <vasanthfeb18@gmail.com>
2024-07-21 22:06:34 +0530

    sslc-result master

---
+++ script.js
@@ -0,0 +1,22 @@
+// name start
+function fun(){
+    
+let fn=document.getElementById("full_name_in").value;
+
+document.getElementById("full_name_out").innerHTML=fn;
+
+//name end
+
+let t=document.getElementById("t-in").value;
+let e=document.getElementById("e-in").value;
+let m=document.getElementById("m-in").value;
+let s=document.getElementById("s-in").value;
+let ss=document.getElementById("ss-in").value;
+
+document.getElementById("t-out").innerHTML=t;
+document.getElementById("e-out").innerHTML=e;
+document.getElementById("m-out").innerHTML=m;
+document.getElementById("s-out").innerHTML=s;
+document.getElementById("ss-out").innerHTML=ss;
+
+}
